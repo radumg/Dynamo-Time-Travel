9f3da3e6284a8c83b44cdae7f3677151c98e175f
Radu Gidei <radugidei@gmail.com>
2019-08-05 10:15:52 +0100

    introduce NodePreset class to hold input/output data

---
+++ TimeTravel.Tests/src/NodePresetTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TimeTravel;
+
+namespace TimeTravel.Tests
+{
+    [TestClass]
+    public class NodePresetTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // Arrange
+            var nodePreset = new NodePreset();
+
+            // Act
+
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
---
+++ TimeTravel/src/NodePreset.cs
@@ -0,0 +1,34 @@
+﻿using Dynamo.Graph.Nodes;
+using Dynamo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTravel
+{
+    public class NodePreset
+    {
+        public string Id { get; set; }
+        public string NodeType { get; set; }
+        public NodeInputData InputData { get; set; }
+        public NodeOutputData OutputData { get; set; }
+
+        public NodePreset()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public NodePreset(NodeModel nodeModel):this()
+        {
+            if (nodeModel == null)
+                throw new ArgumentNullException(nameof(nodeModel));
+
+            this.Id = nodeModel.GUID.ToString();
+            this.NodeType = nodeModel.NodeType;
+            this.InputData = nodeModel.InputData;
+            this.OutputData = nodeModel.OutputData;
+        }
+    }
+}
